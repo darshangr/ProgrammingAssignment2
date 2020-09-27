4ae7975e9bed153951a7fff0113d7a8095e4d429
Sudarshan <>
2020-09-26 23:07:18 -0400

    Changes for caching the inverse of a vector.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  reverse <- NULL
+  set <- function(y) {
+    x <<- y
+    reverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) reverse <<- inverse
+  getinverse <- function() reverse
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  reverse <- x$getinverse()
+  if (!is.null(reverse)) {
+    message("from cache")
+    return(reverse)
+  }
+  data <- x$get()
+  reverse <- solve(data, ...)
+  x$setinverse(reverse)
+  reverse
 }
